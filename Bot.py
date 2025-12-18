3a5f46b0e3dd897a1d06ef487bc74e4979fa9bc4
fevdwef <dwffvef@gmail.com>
2025-12-18 21:18:25 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ import time
 
 url = input("url: ")
 duration = int(input(" Time space: ")
-int x = input(" how many views: ")
+x =int(input(" how many views: "))
 
 for i in range(x):
   webbrowser.open_new(url)
