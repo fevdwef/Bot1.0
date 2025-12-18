093c8626e14f91b104f0aa59cf692ae936030adf
fevdwef <dwffvef@gmail.com>
2025-12-18 21:14:00 +0300

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+
+url = input("url: ")
+duration = int(input(" Time space: ")
+int x = input(" how many views: ")
+
+for i in range(x):
+  webbrowser.open_new(url)
+  time.sleep(duration)
+               
